d2e4dda9d14ee83ecc19e2f2625497d0c825470b
Daniel Hoelbling <tigraine@tigraine.at>
2011-04-08 22:55:14 +0200

    Initial commit

---
+++ Clip/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Clip
+{
+    using System;
+    using System.Windows.Forms;
+
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            try 
+            {
+                var consoleInput = Console.In.ReadToEnd();
+                Console.WriteLine("Copied to Clipboard\n{0}", consoleInput);
+                Clipboard.SetText(consoleInput);
+            } 
+            catch(Exception ex)
+            {
+                Console.WriteLine("Something went terribly wrong\n{0}", ex.Message);
+            }
+        }
+    }
+}
